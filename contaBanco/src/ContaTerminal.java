a5aac4fd50e76c53af32843c7a483c78da055536
Eliothluy <eliothluy@gmail.com>
2024-08-02 16:27:04 -0300

    O desafio contaBanco já pronto.

--- contaBanco/src/ContaTerminal.java
+++ contaBanco/src/ContaTerminal.java
@@ -1,9 +1,35 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class ContaTerminal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Locale, para o padrão norte americano + a classe Scanner
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 
+		// Entrada do número da Agencia, com a declaração da variável numeroAgencia
+		System.out.print("Por favor, digite o número da Agência: ");
+		int numeroAgencia = sc.nextInt();
+
+		// Entrada da Agência, com a declaração da variável agencia
+		System.out.print("Por favor, digite a Agência: ");
+		String agencia = sc.next();
+
+		// Entrada do nome do cliente, com um nextLine para limpar o "Enter" da entrada
+		System.out.print("Por favor, digite o nome do cliente: ");
+		sc.nextLine();
+		String nomeCliente = sc.nextLine();
+
+		// Entrada do saldo da Conta, com a declaração da variável
+		System.out.print("Por favor, digite o saldo da conta: ");
+		double saldoConta = sc.nextDouble();
+
+		System.out.printf(
+				"Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque\n",
+				nomeCliente, agencia, numeroAgencia, saldoConta);
+
+		sc.close();
 	}
 
 }
